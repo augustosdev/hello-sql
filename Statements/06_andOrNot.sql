7d72ef450b28b31b42828110af67a380c55d8358
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-05-08 20:08:59 -0300

    and or not limit

---
+++ Statements/06_andOrNot.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users WHERE NOT mail = 'sara@gmail.com'
+
+SELECT * FROM users WHERE NOT mail = 'sara@gmail.com' AND age = 15;
+
+SELECT * FROM users WHERE NOT mail = 'sara@gmail.com' OR age = 15;
