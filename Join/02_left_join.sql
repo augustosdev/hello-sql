11963afb8d1bdc6f22afbac832d6f0e2daed969b
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-05-29 18:37:29 -0300

    right join

--- Join/02_left_join.sql
+++ Join/02_left_join.sql
@@ -5,3 +5,17 @@ ON users.user_id = dni.user_id;
 SELECT name, dni_number FROM users
 LEFT JOIN dni
 ON users.user_id = dni.user_id;
+
+SELECT users.name, languages.name 
+FROM users
+LEFT JOIN users_languages ON users.user_id = users_languages.user_id
+LEFT JOIN languages ON users_languages.language_id = languages.language_id
+
+SELECT * FROM users
+RIGHT JOIN dni
+ON users.user_id = dni.user_id;
+
+SELECT users.name, languages.name 
+FROM users
+RIGHT JOIN users_languages ON users.user_id = users_languages.user_id
+RIGHT JOIN languages ON users_languages.language_id = languages.language_id
