fa3a702eba8f105e8dbc1d877641d9062e8c3c8a
Samuel Augusto Alvarado <sam_zoom@hotmail.com>
2024-05-29 18:57:18 -0300

    union

--- Join/02_left_join.sql
+++ Join/02_left_join.sql
@@ -11,11 +11,3 @@ FROM users
 LEFT JOIN users_languages ON users.user_id = users_languages.user_id
 LEFT JOIN languages ON users_languages.language_id = languages.language_id
 
-SELECT * FROM users
-RIGHT JOIN dni
-ON users.user_id = dni.user_id;
-
-SELECT users.name, languages.name 
-FROM users
-RIGHT JOIN users_languages ON users.user_id = users_languages.user_id
-RIGHT JOIN languages ON users_languages.language_id = languages.language_id
---
+++ Join/04_union.sql
@@ -0,0 +1,19 @@
+SELECT users.user_id AS u_user_id, dni_user_id AS d_user_id
+FROM users
+LEFT JOIN dni
+ON users.user_id = dni.user_id
+UNION ALL 
+SELECT users.user_id AS user_id, dni.user_id AS d_user_id
+FROM users
+RIGHT JOIN dni 
+ON users.user_id = dni.user_id;
+
+SELECT *
+FROM users
+LEFT JOIN dni
+ON users.user_id = dni.user_id
+UNION ALL 
+SELECT *
+FROM users
+RIGHT JOIN dni 
+ON users.user_id = dni.user_id;
